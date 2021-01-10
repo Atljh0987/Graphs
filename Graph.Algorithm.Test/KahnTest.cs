74e4322872232c77307e7b6a89e6823676143b3d
Atljh0987 <example@example.com>
2021-01-10 22:26:52 +0500

    Add Kahn tests

--- Graph.Algorithm.Test/KahnTest.cs
+++ Graph.Algorithm.Test/KahnTest.cs
@@ -2,6 +2,7 @@
 using System;
 using Graphs;
 using Graphs.Algorithms;
+using System.Collections.Generic;
 
 namespace Graph.Algorithm.Test
 {
@@ -19,6 +20,98 @@ namespace Graph.Algorithm.Test
         public void NullCheck()
         {
             Kahn.Run(null);
+        }
+
+        [TestMethod]
+        public void WithoutArcs()
+        {
+            UnweightedGraphMatrix graph = new UnweightedGraphMatrix(true, 5);
+            int[] result = Kahn.Run(graph);
+
+            Assert.AreEqual(5, result.Length);
+            Assert.AreEqual(0, result[0]);
+            Assert.AreEqual(1, result[1]);
+            Assert.AreEqual(2, result[2]);
+            Assert.AreEqual(3, result[3]);
+            Assert.AreEqual(4, result[4]);
+        }
+
+        [TestMethod]
+        public void ThreePeaks()
+        {
+            UnweightedGraphMatrix graph = new UnweightedGraphMatrix(true, 3);
+            graph.AddArc(1, 0);
+            graph.AddArc(1, 2);
+
+            graph.AddArc(0, 2);
+
+            Assert.AreEqual(1, Kahn.Run(graph)[0]);
+            Assert.AreEqual(0, Kahn.Run(graph)[1]);
+            Assert.AreEqual(2, Kahn.Run(graph)[2]);
+        }
+
+        [TestMethod]
+        public void FivePeaks()
+        {
+            UnweightedGraphMatrix graph = new UnweightedGraphMatrix(true, 5);
+            graph.AddArc(0, 1);
+            graph.AddArc(0, 2);
+            graph.AddArc(0, 3);
+            graph.AddArc(0, 4);
+
+            graph.AddArc(1, 3);
+
+            graph.AddArc(2, 3);
+            graph.AddArc(2, 4);
+
+            graph.AddArc(3, 4);
+
+            Assert.AreEqual(0, Kahn.Run(graph)[0]);
+            Assert.AreEqual(1, Kahn.Run(graph)[1]);
+            Assert.AreEqual(2, Kahn.Run(graph)[2]);
+            Assert.AreEqual(3, Kahn.Run(graph)[3]);
+            Assert.AreEqual(4, Kahn.Run(graph)[4]);
+        }
+
+        [TestMethod]
+        public void FivePeaksSeveralScatter()
+        {
+            // как разобрался, не увидел сложности в таких тестрах, на каждом уровне вершины просто в порядке возрастания            
+            UnweightedGraphMatrix graph = new UnweightedGraphMatrix(true, 5); 
+            graph.AddArc(0, 1);
+            graph.AddArc(0, 2);
+
+            graph.AddArc(1, 2);
+
+            graph.AddArc(3, 1);
+
+            graph.AddArc(4, 2);
+            graph.AddArc(4, 1);
+
+            Assert.AreEqual(0, Kahn.Run(graph)[0]);
+            Assert.AreEqual(3, Kahn.Run(graph)[1]);
+            Assert.AreEqual(4, Kahn.Run(graph)[2]);
+            Assert.AreEqual(1, Kahn.Run(graph)[3]);
+            Assert.AreEqual(2, Kahn.Run(graph)[4]);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void CycleSix()
+        {
+            UnweightedGraphMatrix graph = new UnweightedGraphMatrix(true, 6);
+            graph.AddArc(0, 3);
+            graph.AddArc(0, 2);
+
+            graph.AddArc(1, 3);
+
+            graph.AddArc(3, 4);
+            graph.AddArc(3, 5);
+            
+            graph.AddArc(4, 1);
+
+            graph.AddArc(5, 1);
+
+            Kahn.Run(graph);
         }
     }
 }
--- Graphs/Algorithms/Kahn.cs
+++ Graphs/Algorithms/Kahn.cs
@@ -14,30 +14,91 @@ namespace Graphs.Algorithms
             if (graph == null) throw new ArgumentNullException("graph");
             if (!graph.Oriented) throw new ArgumentException("Graph must be oriented");
 
-            return null;
+            using(UnmanagedNumbersArray CountOutputArcs = new UnmanagedNumbersArray(graph.PeakCount), 
+                                         SortedElements = new UnmanagedNumbersArray(graph.PeakCount))
+            {
+                int sortedElementsCount = 0;
+                int previousPassageCount = 0;
+
+                for(int i = 0; i < CountOutputArcs.Length; i++)
+                {
+                    CountOutputArcs[i] = graph.InComingArcs(i).Count(); // !!!!
+                    if (CountOutputArcs[i] == 0)
+                        SortedElements[sortedElementsCount++] = i;
+                }
+
+                while (sortedElementsCount < CountOutputArcs.Length) // Пока есть несортированные вершины
+                {
+                    int sortedInThisPass = 0;
+
+                    for (int i = previousPassageCount; i < sortedElementsCount; i++) // Проходим по всем отсортированным вершинам
+                    {
+                        //foreach (var el in graph.OutGoingArcs(i)) // Перебираем ребра идущие из отсортированных // ошибка
+                        foreach (var el in graph.OutGoingArcs(SortedElements[i])) 
+                            {
+                            CountOutputArcs[el]--;
+                            if (CountOutputArcs[el] == 0)
+                                SortedElements[sortedElementsCount + sortedInThisPass++] = el;
+                        }
+                    }
+
+                    if (sortedInThisPass == 0)
+                        throw new ArgumentException();
+
+                    previousPassageCount = sortedElementsCount;
+                    sortedElementsCount += sortedInThisPass;
+                }
+
+                return SortedElements.ToArray();
+            }
         }
-    }
+    }   
 
     unsafe class UnmanagedMemoryArray : IDisposable
     {
-        public readonly int Length;
+        public readonly int Size;
         protected readonly void* pointer; // указатель
         public UnmanagedMemoryArray(int length)
         {
             pointer = Marshal.AllocHGlobal(length).ToPointer(); // Marshal класс взаимодействия с неуправл кодом // AllocHGl место в неупр памяти длинны length и перевести в Pointer
-            if (pointer != null) { 
-                Length = length;
-                GC.AddMemoryPressure(Length); // GC сборщик мусора // AddMemoryPressute информируем что выделен блок длинны length
+            if (pointer != null) {
+                Size = length;
+                GC.AddMemoryPressure(Size); // GC сборщик мусора // AddMemoryPressute информируем что выделен блок длинны length
             }
         }
 
         public void Dispose()
         {
-            if(Length > 0)
+            if(Size > 0)
             {
                 Marshal.FreeHGlobal((IntPtr)pointer); // FreeHG освобождение памяти по указателю pointer
-                GC.RemoveMemoryPressure(Length); // Сообщение СБ мусора что память освобожденаfffffffff
+                GC.RemoveMemoryPressure(Size); // Сообщение СБ мусора что память освобождена
             }
+        }
+    }
+
+    unsafe class UnmanagedNumbersArray : UnmanagedMemoryArray
+    {
+        public UnmanagedNumbersArray(int size) : base(size * sizeof(int)) { }
+
+        public int Length => Size / sizeof(int);
+
+        public int this[int index]
+        {
+            get => ((int*)pointer)[index];
+            set => ((int*)pointer)[index] = value;
+        }
+
+        public int[] ToArray()
+        {
+            int[] result = new int[Length];
+
+            for(int i = 0; i < result.Length; i++)
+            {
+                result[i] = this[i];
+            }
+
+            return result;
         }
     }
 }
